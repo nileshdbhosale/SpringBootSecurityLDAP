c5e26a41b14782ee07f0abc5bf1b8046e4152494
Nilesh Bhosale <Nilesh@Nileshs-MacBook-Air.local>
2020-07-22 12:11:01 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Once application is up and running, go to 
+http://localhost:8080/hello
+
+enter 
+username: ben
+password: benspassword
---
+++ src/main/java/com/user/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello User, You are authorized !";
+	}
+}
